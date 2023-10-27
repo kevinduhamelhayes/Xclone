8ee17b55b5e6f83f7ea500190cab457d7cc97d18
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-10-27 03:32:17 -0300

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,11 @@ export const metadata: Metadata = {
   description: 'Generado moviendo las manintas'
 }
 
-export default function RootLayout ({ children }: { children: React.ReactNode }) {
+export default function RootLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en" className='dark'>
       <body>
