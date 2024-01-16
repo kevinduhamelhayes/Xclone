b59a848425e9f3da327c82b0473c66d8522b384b
kevinduhamelhayes <kevinduhamelh@gmail.com>
2024-01-15 23:14:27 -0300

    add styles in form

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -12,20 +12,23 @@ export function ComposePost ({
   const formRef = useRef<HTMLFormElement>(null)
 
   return (
-    <form ref={formRef} action={async (formData) => {
+<form ref={formRef} action={async (formData) => {
       await addPost(formData)
       formRef.current?.reset()
-    }} className='flex flex-row p-3 border-b border-white/20'>
-      <img className='rounded-full w-10 h-10 object-contain mr-4' src={userAvatarUrl} />
+    }} className='flex flex-row p-3 border-b border-white/20 shadow-lg bg-gray-800'>
+      <img className='rounded-full w-10 h-10 object-cover mr-4' src={userAvatarUrl} />
       <div className='flex flex-1 flex-col gap-y-4'>
       <textarea
         name='content'
         rows={4}
-        className='w-full text-xl bg-black placeholder-gray-500 p-2'
+        className='w-full text-xl bg-black text-white placeholder-gray-300 p-3 border rounded-lg'
         placeholder='¡¿Qué está pasando!?'
       ></textarea>
-        <ComposePostButton />
+        <button className='py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'>
+          Publicar
+        </button>
       </div>
     </form>
+
   )
 }
